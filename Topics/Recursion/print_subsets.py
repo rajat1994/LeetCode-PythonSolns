a48a354b9253b9c156359155f6f6143d29c47a1a
rajat1994 <rajatarora25@yahoo.in>
2021-06-04 22:17:31 +0530

    print subsets

---
+++ Topics/Recursion/print_subsets.py
@@ -0,0 +1,18 @@
+
+def subsets (ip, op):
+    if len(ip) == 0:
+        print(op)
+        return
+
+    op1 = op
+    op2 = op
+
+    op2 = op2+ip[0]
+    ip = ip[1:]
+
+    subsets(ip,op1)
+    subsets(ip,op2)
+
+
+subsets("123","")
+
